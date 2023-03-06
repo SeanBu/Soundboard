ea7bb395ead06d5e0562e733657924c7b6297474
seanb <seanbuchas@gmail.com>
2023-03-05 21:14:48 -0500

    Add components for second audio file

--- Soundboard/Form1.Designer.cs
+++ Soundboard/Form1.Designer.cs
@@ -33,7 +33,13 @@
             playBtn1 = new Button();
             volume1 = new TrackBar();
             stop1 = new Button();
+            textBox1 = new TextBox();
+            trackBar1 = new TrackBar();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
             ((System.ComponentModel.ISupportInitialize)volume1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
             SuspendLayout();
             // 
             // button1
@@ -84,11 +90,61 @@
             stop1.UseVisualStyleBackColor = true;
             stop1.Click += stop1_Click;
             // 
+            // textBox1
+            // 
+            textBox1.Enabled = false;
+            textBox1.Location = new Point(12, 92);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(253, 23);
+            textBox1.TabIndex = 5;
+            textBox1.Text = "No audio clip selected (.mp3)";
+            // 
+            // trackBar1
+            // 
+            trackBar1.Location = new Point(12, 121);
+            trackBar1.Maximum = 100;
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(253, 45);
+            trackBar1.TabIndex = 6;
+            trackBar1.Value = 75;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(271, 92);
+            button2.Name = "button2";
+            button2.Size = new Size(139, 23);
+            button2.TabIndex = 7;
+            button2.Text = "Load Audio File";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(271, 121);
+            button3.Name = "button3";
+            button3.Size = new Size(65, 23);
+            button3.TabIndex = 8;
+            button3.Text = "Play";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(342, 121);
+            button4.Name = "button4";
+            button4.Size = new Size(68, 23);
+            button4.TabIndex = 9;
+            button4.Text = "Stop";
+            button4.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(887, 450);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(trackBar1);
+            Controls.Add(textBox1);
             Controls.Add(stop1);
             Controls.Add(volume1);
             Controls.Add(playBtn1);
@@ -98,6 +154,7 @@
             Text = "Form1";
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)volume1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -109,5 +166,10 @@
         private Button playBtn1;
         private TrackBar volume1;
         private Button stop1;
+        private TextBox textBox1;
+        private TrackBar trackBar1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
     }
 }
